f68f194f8693d439baaf960868417e7d36faca6b
alommdkhushbu-design <alommdkhushbu@gmail.com>
2026-09-03 00:00:04 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return 'Server is running successfully!'
+
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
